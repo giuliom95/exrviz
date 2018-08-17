216e283a5c7faae096c721c2bafabf9a7aa6f592
giuliom95 <giuliomartella1995@gmail.com>
2018-08-17 15:26:02 +0200

    EXR basic loading

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <vector>
+#include <array>
 
 #include <ImfRgbaFile.h>
 #include <ImfRgba.h>
@@ -8,20 +9,34 @@
 
 #include "oglWidget.hpp"
 
-int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
-    QWidget window;
-    window.resize(320, 240);
-    window.setWindowTitle("Hello Qt5!");
-
-    QVBoxLayout* mainLayout = new QVBoxLayout;
-    mainLayout->setContentsMargins(0,0,0,0);
-
-    OGLWidget* oglWidget = new OGLWidget;
-    
-    mainLayout->addWidget(oglWidget);
-    window.setLayout(mainLayout);
-    window.show();
-
-    return app.exec();
+void read_exr(	const char* fileName,
+				std::vector<Imf::Rgba>& image,
+				int& width, int& height) {
+	Imf::RgbaInputFile file(fileName);
+	Imath::Box2i dw = file.dataWindow();
+	width  = dw.max.x - dw.min.x + 1;
+	height = dw.max.y - dw.min.y + 1;
+	image.resize(height * width);
+	file.setFrameBuffer(image.data() - dw.min.x - dw.min.y * width, 1, width);
+	file.readPixels(dw.min.y, dw.max.y);
 }
+
+
+int main(int argc, char** argv) {
+	QApplication app(argc, argv);
+	QWidget window;
+	window.resize(320, 240);
+	window.setWindowTitle("Hello Qt5!");
+
+	QVBoxLayout* mainLayout = new QVBoxLayout;
+	mainLayout->setContentsMargins(0,0,0,0);
+
+	OGLWidget* oglWidget = new OGLWidget;
+	
+	mainLayout->addWidget(oglWidget);
+	window.setLayout(mainLayout);
+	window.show();
+
+	return app.exec();
+}
+
--- src/oglWidget.cpp
+++ src/oglWidget.cpp
@@ -1,28 +1,29 @@
 #include "oglWidget.hpp"
 
 void OGLWidget::initializeGL() {
-    std::cout << "Hello OpenGL!" << std::endl;
-    initializeOpenGLFunctions();
-    glClearColor(0.0f, 0.0f, 0.2f, 1.0f);
+	std::cout << "Hello OpenGL!" << std::endl;
+	initializeOpenGLFunctions();
+	glClearColor(0.0f, 0.0f, 0.2f, 1.0f);
 }
 
 void OGLWidget::resizeGL(int w, int h) {
-    std::cout << w << " " << h << std::endl;
+	//std::cout << w << " " << h << std::endl;
 
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(0, w, h, 0, -1, +1);
-    glMatrixMode( GL_MODELVIEW ); 
-    glLoadIdentity();
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0, w, h, 0, -1, +1);
+	glMatrixMode( GL_MODELVIEW ); 
+	glLoadIdentity();
 }
 
 void OGLWidget::paintGL() {
-    std::cout << "Painting..." << std::endl;
-    glClear(GL_COLOR_BUFFER_BIT);
-    glBegin( GL_QUADS ); 
-        glVertex2f(   0,   0 );
-        glVertex2f( 200,   0 );
-        glVertex2f( 200, 200 );
-        glVertex2f(   0, 200 );
-    glEnd();
+	//std::cout << "Painting..." << std::endl;
+	
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin( GL_QUADS ); 
+		glVertex2f(   0,   0 );
+		glVertex2f( 200,   0 );
+		glVertex2f( 200, 200 );
+		glVertex2f(   0, 200 );
+	glEnd();
 }
